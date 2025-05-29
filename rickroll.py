cc95225c926662a7f5bd51a7907e42b9b4e762cd
kaikub <thunwa.pungtam@gmail.com>
2025-05-29 11:00:49 +0700

    Trust me! It a Free Robux!!

---
+++ rickroll.py
@@ -0,0 +1,15 @@
+import webbrowser as wb
+import time
+delay = 1 #seconds
+x=int(input('How many Robux do u want?'))
+y=0
+
+print('Genarate your Robux! 0% complete') 
+time.sleep(delay)
+print('Please wait...')
+time.sleep(delay)
+print('Genarated! 100% complete')
+
+while x>y:
+    wb.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    y=y+1
